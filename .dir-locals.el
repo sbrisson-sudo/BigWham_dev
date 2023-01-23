206ceba45562f16dd220cadbc8a2acc39045e525
ankit dabba <ankit.gupta@epfl.ch>
2023-01-23 21:05:46 +0100

    will remove set from griffith example

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,7 +3,7 @@
 
 (
  ;; (python-mode . ((pyvenv-workon . "myscipy")))
- (nil . ((compile-command . (concat "source bigwham_venv/bin/activate;. /opt/intel/mkl/bin/mklvars.sh intel64; cd build; cmake -DCMAKE_CXX_COMPILER=g++ -DBUILD_GOOGLE_TESTS=1 -DBUILD_PYTHON_BINDINGS=1 -DUSE_INTEL=0 -DIL_OPENMP=1 -DIL_OPENBLAS=0 -DIL_MKL=1  ..;make -j2; ./BigWhamElastUnitTest"))))
+ (nil . ((compile-command . (concat "cd build; make -j2"))))
 
 ;; (nil
 ;;   (format-all-formatters
--- src/examples/griffithcrack-2d.cpp
+++ src/examples/griffithcrack-2d.cpp
@@ -12,44 +12,64 @@
 ///
 ///=============================================================================
 
-
-#include <iostream>
 #include <Eigen/Dense>
+#include <iostream>
+#include <memory>
+#include <string>
 #include <vector>
 
-using namespace Eigen;
+#include "BigWham.h"
+#include <il/Array2D.h>
 
-using Int2D = Array<int, 2, Dynamic>;
-using Real2D = Array<double, 2, Dynamic>;
+using namespace Eigen;
+using Int2D = Array<int, Dynamic, Dynamic, RowMajor>;
+using Real2D = Array<double, Dynamic, Dynamic, RowMajor>;
 using Real = double;
 
 int main(int argc, char *argv[]) {
 
-  int nelems = 70;
+  // Using 2DP1 element
+
+  int nelems = 5;
   int dim = 2;
+
+  il::Array2D<double> Coor{nelems + 1, dim, 0.};
+  il::Array2D<il::int_t> Conn{nelems, 2, 0};
 
   Real E = 1.0;
   Real nu = 0.2;
+  std::vector<Real> properties = {E, nu};
 
+  std::vector<Real> pts;
+  std::vector<int> conn;
 
-  Real2D pts;
-  Int2D conn;
+  std::string kernel = "2DP1";
 
+  int nleaf = 16;
+  Real eta = 30.0;
+  Real eps_aca = 0.0001;
 
-  pts.setZero(nelems+1, dim);
-  conn.setZero(nelems, dim);
+  pts.resize((nelems + 1) * dim);
+  conn.resize(nelems * dim);
 
-  for (int i = 0; i < nelems+1; ++i) {
-    pts(i,0) = -1.0 + (2.0 * i) / nelems;
-    pts(i,1) = 0.;
+  for (int i = 0; i < nelems + 1; ++i) {
+    pts[i * dim + 0] = -1.0 + (2.0 * i) / nelems;
+    pts[i * dim + 1] = 0.;
   }
 
   for (int i = 0; i < nelems; ++i) {
-    conn(i,0) = i;
-    conn(i,1) = i+1;
+    conn[i * dim + 0] = i;
+    conn[i * dim + 1] = i + 1;
   }
 
-  std::cout << "Hello" << std::endl;
+  // auto hmat = std::unique_ptr<Bigwhamio>();
+
+  Bigwhamio hmat;
+
+  std::cout << Map<Real2D>(pts.data(), nelems + 1, dim) << std::endl;
+  std::cout << Map<Int2D>(conn.data(), nelems, dim) << std::endl;
+
+  hmat.set(pts, conn, kernel, properties, nleaf, eta, eps_aca);
 
   return 0;
 }
