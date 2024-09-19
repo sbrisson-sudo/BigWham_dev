b29a34dffe0dfe8d42080f9f7ad91b5c301515f5
ankit-madariaga <ankit.gupta@ens.fr>
2024-09-19 16:37:08 +0200

    removed runtime dispatch issue in julia interface

--- interfaces/julia/bigwham4jl.jl
+++ interfaces/julia/bigwham4jl.jl
@@ -25,44 +25,53 @@ end
 
 using LinearMaps, LinearAlgebra
 
-struct BEMatrix <: LinearMaps.LinearMap{Float64}
-    bigwham_obj::BigWhamIO
+struct BEMatrix{B<:BigWhamIO} <: LinearMaps.LinearMap{Float64}
+    bigwham_obj::B
     col_pts::Vector{Float64}
     normals::Vector{Float64}
     rotation_matrix::Vector{Float64}
     size::Dims{2}
 
-    function BEMatrix(coor::Vector{Float64}, conn::Vector{Int32}, kernel::String, prop::Vector{Float64})
+    function BEMatrix(coor::Vector{Float64}, conn::Vector{Int32},
+        kernel::String, prop::Vector{Float64})
         h = BigWhamIO(coor, conn, kernel, prop)
         colpts = get_collocation_points(h)
         normals = get_element_normals(h)
         rotation_matrix = get_rotation_matrix(h)
         dim = size(colpts)[1]
-        return new(h, colpts, normals, rotation_matrix, (dim, dim))
+        return new{typeof(h)}(h, colpts, normals, rotation_matrix, (dim, dim))
     end
 
-    function BEMatrix(coor::Matrix{Float64}, conn::Matrix{Int64}, kernel::String, prop::Vector{Float64})
-        coor = flatten_c_order(coor)
-        conn = convert(Vector{Int32}, flatten_c_order(conn))
-        return BEMatrix(coor, conn, kernel, prop)
-    end
+end
+
+
+function BEMatrix(coor::Matrix{Float64}, conn::Matrix{Int64},
+    kernel::String, prop::Vector{Float64})
+    coor = flatten_c_order(coor)
+    conn = convert(Vector{Int32}, flatten_c_order(conn))
+    return BEMatrix(coor, conn, kernel, prop)
 end
 
 Base.size(hmat::BEMatrix) = hmat.size
 
-function LinearMaps._unsafe_mul!(y, A::BEMatrix, x::AbstractVector)
+function LinearMaps._unsafe_mul!(y::AbstractVector, A::BEMatrix, x::AbstractVector)
     tmp = matvec(A.bigwham_obj, x[:])
     copyto!(y, tmp)
 end
 
-import Base: *
-function *(hmat::BEMatrix, dd::Vector{Float64})::Vector{Float64}
-    return matvec(hmat.bigwham_obj, dd)
+function LinearMaps._unsafe_mul!(y::AbstractVector, A::BEMatrix, x::Vector{Float64})
+    tmp = matvec(A.bigwham_obj, x)
+    copyto!(y, tmp)
 end
 
-function *(hmat::BEMatrix, dd::AbstractVector)::Vector{Float64}
-    return matvec(hmat.bigwham_obj, dd[:])
-end
+# import Base: *
+# function *(hmat::BEMatrix, dd::Vector{Float64})::Vector{Float64}
+#     return matvec(hmat.bigwham_obj, dd)
+# end
+
+# function *(hmat::BEMatrix, dd::AbstractVector)::Vector{Float64}
+#     return matvec(hmat.bigwham_obj, dd[:])
+# end
 
 function build_hierarchical_matrix(hmat::BEMatrix, max_leaf_size::Int64, eta::Float64, eps_aca::Float64)
     build_hierarchical_matrix(hmat.bigwham_obj, max_leaf_size, eta, eps_aca)
