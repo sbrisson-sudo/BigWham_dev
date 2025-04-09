ec8af59696041bcc18303a560d7b6228c04e0932
sbrisson-sudo <sylvain.brisson@ens.psl>
2025-04-09 09:49:19 +0200

    typo correction

--- src/io/bigwham_io.cc
+++ src/io/bigwham_io.cc
@@ -108,7 +108,7 @@ BigWhamIO::BigWhamIO(const std::vector<double> &coor,
         // Ensuring we have more OpenMP threads than GPUs
         if (this->n_openMP_threads_ <= num_GPUs_){
 
-            std::cerr << "[ERROR] Less OpenMP threads than GPUs (" << this->n_openMP_threads  << " OpenMP threads and " << num_GPUs_<< " GPUs) aborting..." << std::endl;
+            std::cerr << "[ERROR] Less OpenMP threads than GPUs (" << this->n_openMP_threads_  << " OpenMP threads and " << num_GPUs_<< " GPUs) aborting..." << std::endl;
             std::abort();
         }
 
